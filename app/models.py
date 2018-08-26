9c00819a238aa2c8a868640465235c763a9b44f3
= <=>
2018-08-26 14:47:16 +0100

    Role table with Permissions.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,68 @@
 from app import db
 
+class Permission:
+    ASK_QUESTIONS = 0x01
+    ANSWER_QUESTIONS = 0x02
+    SAVE_HISTORY = 0x04
+    MANAGE_CLASS = 0x08
+    MODERATE_QUESTIONS = 0x10
+    ADMINISTRATOR = 0xff
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Student': (Permission.ASK_QUESTIONS |
+                        Permission.ANSWER_QUESTIONS |
+                        Permission.SAVE_HISTORY, True),
+            'Teacher': (Permission.ASK_QUESTIONS |
+                        Permission.ANSWER_QUESTIONS |
+                        Permission.SAVE_HISTORY |
+                        Permission.MANAGE_CLASS, False),
+            'Moderator': (Permission.ASK_QUESTIONS |
+                          Permission.ANSWER_QUESTIONS |
+                          Permission.SAVE_HISTORY |
+                          Permission.MODERATE_QUESTIONS, False),
+            'Administrator': (0xff, False)
+        }
+
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(120), unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
+
+    def is_admin(self):
+        return self.can(Permission.ADMINISTRATOR)
+    
+    def can(self, permissions):
+        return self.role is not None and (self.role.permissions & permissions) == permissions
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
