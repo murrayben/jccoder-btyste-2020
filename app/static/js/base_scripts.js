2be43a906be90c5217f43f6064e72394ea0f1b71
Benjamin <=>
2019-11-18 21:20:56 +0000

    Added nice footer, changed navbar and other small things

--- app/static/js/base_scripts.js
+++ app/static/js/base_scripts.js
@@ -1,9 +1,3 @@
-$('#scroll-top').hide();
-$('#scroll-top').on('click', function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000);
-});
 $(document).on('scroll', function () {
     if ($(window).scrollTop() > 100) {
         $("#scroll-top").fadeIn(500);
@@ -12,7 +6,24 @@ $(document).on('scroll', function () {
     }
 });
 $(function () {
+    $('#scroll-top').hide();
+    $('#scroll-top').on('click', function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
     $('[data-toggle="popover"]').popover({
         container: 'body'
     });
-  })
+
+    $(window).on('resize', adjustFooter);
+    adjustFooter();
+
+    function adjustFooter() {
+        if ($(document).width() <= 992) {
+            $('footer').css('height', 'auto');
+            $('footer').css('marginTop', -1 * $('footer').outerHeight());
+            $('#content').css('padding-bottom', $('footer').outerHeight());
+        }
+    }   
+})
